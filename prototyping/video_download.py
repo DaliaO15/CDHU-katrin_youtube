138fd7a97d9d74795da130d72d5a4eaa935c692a
DaliaO15 <dalia141503@gmail.com>
2023-01-02 16:08:45 +0100

    Video download code done

--- prototyping/video_download.py
+++ prototyping/video_download.py
@@ -16,11 +16,10 @@ def create_folder(folder):
     try: # if it is possible to create the folder
         os.mkdir(folder)
     except: # if the folder was not created successfuly, raise a warning
-        print('There is an error with the folder')
+        logging.info('There is an error with the folder')
 
 def video_download_tube(youtube_video, url, path):
     # Create a youtube object to use the API
-    #youtube_video = YouTube(url)
     video_id = extract.video_id(url)
     itag = youtube_video.streams.filter(only_video=True).asc().first().itag # get the highest resolution possible 
     stream = youtube_video.streams.get_by_itag(itag)
@@ -32,22 +31,19 @@ def video_download_dl(url, path):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(['https://www.'+ url]) # Downloads the best format by default 
  
-def video_download(df): # df must contain a column called url where the urls to the videos are stored
+def video_download(df,root_folder): # df must contain a column called url where the urls to the videos are stored
 
     for idx in range(0,df.shape[0]):
         
         # Access the video url from data frame
         video_url = df['url'].iloc[idx]
-        #print(f'Video_url: {video_url}')
         channel_name = df['channel'].iloc[idx]
 
         # Create a folder for that channel
-        channel_folder = os.getcwd() + '/Videos/' + channel_name + '/'
-        #print(f'Folder: {channel_folder}')
+        channel_folder = root_folder + channel_name + '/'
   
         yt = YouTube(video_url)
         len = yt.length
-        #video_id = extract.video_id(video_url)
         
         # Create a new folder for the channel
         if os.path.exists(channel_folder):
@@ -61,18 +57,19 @@ def video_download(df): # df must contain a column called url where the urls to 
             try:
                 video_download_tube(yt, video_url, channel_folder)
             except:
-                video_download_dl(video_url, channel_folder)
-                #print('Video not downloaded')
+                #video_download_dl(video_url, channel_folder)
+                pass
         else:
             logging.info(f'The video {video_url} exceeded the time constrain. Length {len} seconds.')
        
 
 def main():
     file_test = 'video_urls_FULL.csv'
+    root = '../Data_collection/data/videos/'
     df = pd.read_csv(file_test)
     df = df.drop(columns = 'Unnamed: 0')
     df = df[255:265].reset_index(drop= True)
-    video_metadata = video_download(df) 
+    video_download(df,root) 
 
 if __name__ == "__main__":
     main()
