0f9fe98afbda7040cbaeff67f6bbb629e69aa5a8
DaliaO15 <dalia141503@gmail.com>
2023-01-13 16:56:23 +0100

    download testing

--- prototyping/video_metadata_collection.py
+++ prototyping/video_metadata_collection.py
@@ -6,15 +6,38 @@ from collections import defaultdict
 import os
 import logging
 
-logging.basicConfig(level=logging.DEBUG, filename='../Data_collection/data/logs/video_metadata.log', 
+logging.basicConfig(level=logging.DEBUG, filename='video_metadata.log', 
                     format='%(asctime)s : %(levelname)s : %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 
-def video_metadata_v2(df):
+def itags(youtube_video, url, file):
+
+    try: 
+        itag_lst = youtube_video.streams.filter(only_video=True).asc() # get the highest resolution possible
+
+        if len(itag_lst) >= 2:
+            bests = [int(itag_lst[i].itag) for i in range(0,2)]
+        elif len(itag_lst) == 1:
+            bests = [int(itag_lst.itag), None]
+        else:
+            logging.info(f'The lenght of itag_list is zero for video {url}')
+            bests = [None]*2
+            file.write(f'Url with no itags: {url} \n')
+    except:
+        logging.info(f'Itags not retrieved. Video: {url}')
+        file.write(f'Itags not retrieved. Video: {url} \n')
+        bests = [None]*2
+        
+    return bests
+
+
+def video_metadata(df):
     keys = ['channel', 'url', 'id', 'title', 'author', 'description', 'keywords', 'length_in_secs', 
-            'nbr_views', 'age_resticted', 'publication_date', 'rating']
+            'nbr_views', 'age_resticted', 'publication_date', 'rating','best_itag','sec_itag']
 
     metadata = defaultdict(list)
+    #destino = '../Data_collection/data/'
+    catched_urls = open('catched_urls_v2.txt', 'w')
 
     for idx in range(0,df.shape[0]):
 
@@ -22,39 +45,34 @@ def video_metadata_v2(df):
         video_url = df['url'].iloc[idx]
         channel_name = df['channel'].iloc[idx]
 
-        try:
-            # Create a youtube object to use the API
-            yt = YouTube(video_url)
+        # Create a youtube object to use the API
+        yt = YouTube(video_url)
 
-            # To track the video
-            logging.info( yt.title)
-
-            # Extract metadata
-            info = [channel_name, video_url, extract.video_id(video_url), yt.title, yt.author, yt.description,
-            yt.keywords, yt.length, yt.views, yt.age_restricted, yt.publish_date, yt.rating]
-        except:
-            print('Video could not be accessed')
-            info = [channel_name, video_url]+[None]*10
+        # Get itags
+        itag_lst = itags(yt,video_url,catched_urls)
         
+        # Extract metadata
+        info = [channel_name, video_url, extract.video_id(video_url), yt.title, yt.author, yt.description,
+        yt.keywords, yt.length, yt.views, yt.age_restricted, yt.publish_date, yt.rating, itag_lst[0],itag_lst[1]]
+
         # Store in dictionary 
         for k, v in zip(keys,info):
             metadata[k].append(v)
                     
         # Track 
-        if idx % 200 == 0:
+        if idx % 100 == 0:
             print(f'We are in video {idx} in channel {yt.author}')
 
-    video_metadata = pd.DataFrame.from_dict(metadata)
+    return metadata
 
-    return video_metadata
 
 def main():
     file_test = 'video_urls_FULL.csv'
     df = pd.read_csv(file_test)
-    df = df.drop(columns = 'Unnamed: 0')
-    #df = df[255:265].reset_index(drop= True)
-    video_metadata = video_metadata_v2(df) 
-    video_metadata.to_csv(index=False, path_or_buf='../Data_collection/data/videos_metadata_FULL.csv') 
+    df = df.sample(n= 500, random_state=495).drop(columns='Unnamed: 0')
+    info = video_metadata(df) 
+    info = pd.DataFrame.from_dict(info)
+    info.to_csv(index=False, path_or_buf='videos_metadata_TEST.csv') 
 
 if __name__ == "__main__":
     main()
