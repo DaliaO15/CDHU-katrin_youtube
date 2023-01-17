ef042a84d54cea91b69d555fc8d804162609373c
DaliaO15 <dalia141503@gmail.com>
2023-01-17 16:26:14 +0100

    Changed to parquet file

--- prototyping/video_download.py
+++ prototyping/video_download.py
@@ -2,8 +2,13 @@ import pandas as pd
 import os
 from yt_dlp import YoutubeDL
 import logging
+from collections import defaultdict
+from pathlib import Path
+import os.path
+from fastparquet import write
 
-logging.basicConfig(level=logging.DEBUG, filename='video_download.log', 
+
+logging.basicConfig(level=logging.INFO, filename='video_download.log', 
                     format='%(asctime)s : %(levelname)s : %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 
@@ -16,6 +21,9 @@ def create_folder(folder):
 def video_download(df, root_folder:str): 
     # df: metadata. df must contain a column called url where the urls to the videos are stored
     # root_folder: folder where videos are stored
+    
+    file_path = 'downloads_status.parquet'
+
 
     for idx in range(0,df.shape[0]):
 
@@ -39,22 +47,32 @@ def video_download(df, root_folder:str):
         
         if len <= 600:
             try:
+                collected_status = 'Downloaded'
                 with YoutubeDL(ydl_opts) as ydl:
-                    ydl.download([video_url]) # Downloads the best format by default 
-            #except:
-            #    logging.info(f'Error when downloading video {video_url}')
+                    ydl.download([video_url]) 
 
             except Exception as e:
                 logging.error(f'Error when downloading video {video_url} in channel {channel_name}')
                 logging.error(f'We are in index {idx}')
                 logging.error(f"Error: {e}")
+                collected_status = 'Error'
         else:
             logging.info(f'The video {video_url} exceeded the time constrain. Length {len} seconds.')
             logging.info(f'We are in index {idx}')
+            collected_status = 'Exceed length'
+        
+        # Save downloading status 
+        info = {'Channel':[channel_name],'Video_url':[video_url], 'Status':[collected_status]} 
+
+        if not os.path.isfile(file_path):
+            write(file_path, pd.DataFrame(info))
+        else:
+            write(file_path, pd.DataFrame(info), append=True)
+
 
 def main():
     df = pd.read_parquet('../data_collection/data/videos_metadata.parquet')
-    #df = df.sample(n = 1, random_state=495)
+    #df = df.sample(n = 10, random_state=495)
     video_download(df, root_folder='Videos/') 
 
 if __name__ == "__main__":
