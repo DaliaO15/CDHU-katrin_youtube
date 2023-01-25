88cb0370e89ba35eb99b5b52b61591d4d1d8cd11
DaliaO15 <dalia141503@gmail.com>
2023-01-25 12:12:29 +0100

    Re-try sript

---
+++ prototyping/video_download_re-try.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import os
+from yt_dlp import YoutubeDL
+import logging
+from collections import defaultdict
+from pathlib import Path
+import os.path
+from fastparquet import write
+
+
+logging.basicConfig(level=logging.INFO, filename='video_download.log', 
+                    format='%(asctime)s : %(levelname)s : %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
+
+
+def create_folder(folder):
+    try: # if it is possible to create the folder
+        os.mkdir(folder)
+    except: # if the folder was not created successfuly, raise a warning
+        logging.info('There is an error with the folder')
+
+
+def video_download(df, root_folder:str): 
+    # df: metadata. df must contain a column called url where the urls to the videos are stored
+    # root_folder: folder where videos are stored
+    
+    file_path = 'downloads_status_re-try.parquet'
+
+
+    for idx in range(0,df.shape[0]):
+
+        # Access the video url from data frame
+        video_url = df['Video_url'].iloc[idx]
+        channel_name = df['Channel'].iloc[idx].replace(" ", "_")
+
+        # Create a folder for that channel
+        channel_folder = root_folder + channel_name + '/'
+        
+        # Create a new folder for the channel
+        if not os.path.exists(channel_folder):
+            create_folder(channel_folder)
+
+        ydl_opts = {'outtmpl': channel_folder + '%(id)s-%(format_id)s.%(ext)s',
+                    'format': 'bestvideo[ext=mp4]/mp4',
+                    'overwrites': False,
+                    'retries': 15,
+                    'concurrent-fragments':12}
+        
+        try:
+            collected_status = 'Downloaded'
+            with YoutubeDL(ydl_opts) as ydl:
+                ydl.download(['https://www.' + video_url]) 
+
+        except Exception as e:
+            logging.error(f'Error when downloading video {video_url} in channel {channel_name}')
+            logging.error(f'We are in index {idx}')
+            logging.error(f"Error: {e}")
+            collected_status = 'Error'
+
+        # Save downloading status 
+        info = {'Channel':[channel_name],'Video_url':[video_url], 'Status_2':[collected_status]} 
+ 
+        if not os.path.isfile(file_path):
+            write(file_path, pd.DataFrame(info))
+        else:
+            write(file_path, pd.DataFrame(info), append=True)
+        
+    
+def main():
+    df = pd.read_parquet('error_videos.parquet')
+    #df = df.iloc[0:2]
+    video_download(df, root_folder='Videos/') 
+
+if __name__ == "__main__":
+    main()
