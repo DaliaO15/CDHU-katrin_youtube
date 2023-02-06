d3d529157ad2fe19d5325a139903932aaf8ab068
dalia <dalia141503@gmail.com>
2023-02-06 11:24:31 +0100

    First batch collected

--- Google-Image-Scraper/main.py
+++ Google-Image-Scraper/main.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
  'Forza Italia party Italy']
 
     #Parameters
-    number_of_images = 5                # Desired number of images
+    number_of_images = 200                # Desired number of images
     headless = True                     # True = No Chrome GUI
     min_resolution = (0, 0)             # Minimum desired image resolution
     max_resolution = (9999, 9999)       # Maximum desired image resolution
